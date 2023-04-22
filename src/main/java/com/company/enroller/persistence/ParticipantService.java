4b14f3aedbf8dc09ad9b9ffd6b9f9505593cc34a
WosA <anna.wos@fujitsu.com>
2023-04-22 20:34:38 +0200

    security update

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -46,11 +46,12 @@ public class ParticipantService {
 	}
 
 	public Participant add(Participant participant) {
+		String hashedPassword = passwordEncoder.encode(participant.getPassword());
+		participant.setPassword(hashedPassword);
 		Transaction transaction = connector.getSession().beginTransaction();
 		connector.getSession().save(participant);
 		transaction.commit();
-		String hashedPassword = passwordEncoder.encode(participant.getPassword());
-		participant.setPassword(hashedPassword);
+
 		return participant;
 	}
 
